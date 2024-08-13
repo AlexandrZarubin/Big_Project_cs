9fd6d1658a59ba9bb8c6495f258ef7f9bbb0e491
Alexander Zarubin <whitesashka@gmail.com>
2024-08-13 18:03:05 +0500

    023_ternary_operator

---
+++ 023_ternary_operator/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _023_ternary_operator
+{
+	/*
+	 * Тернарный оператор
+	 * 
+	 * [первый операнд - условие] ? [второй операнд if_true] : [третий операнд if_else]
+	 */
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int a = 0;
+			a++; //унарный 1
+			int b = 1;
+			int c = a + b;//бинарный 2
+
+			bool accessAllowed;
+
+			string storedPassword = "qwerty";
+			string enteredPassword=Console.ReadLine();
+			//if(enteredPassword==storedPassword)
+			//{
+			//	accessAllowed = true;
+			//}
+			//else
+			//{
+			//	accessAllowed = false;
+			//}
+			accessAllowed = storedPassword == enteredPassword ? true : false;
+            Console.WriteLine(accessAllowed);
+            Console.WriteLine();
+
+			int inputDate=int.Parse(Console.ReadLine());
+			int outputDate = (inputDate < 0) ? 0 : inputDate;
+            Console.WriteLine(outputDate);
+
+        }
+	}
+}
