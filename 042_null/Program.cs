79fa39eefcf3014614b4a311d361144ad005a99e
Alexander Zarubin <whitesashka@gmail.com>
2024-09-07 20:59:20 +0500

    add 042

---
+++ 042_null/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ * Оператор условного null
+ */
+namespace _042_null
+{
+	internal class Program
+	{
+		static int[]GetArray()
+		{
+			int[] myArray = null;
+			return myArray;
+		}
+		static void Main(string[] args)
+		{
+			int[]myArray=GetArray();
+			
+            Console.WriteLine("сумма элементов массива: "+(myArray?.Sum()??0));
+			/*
+			if (myArray == null)
+			{
+                Console.WriteLine("сумма элементов массива 0");
+            }
+			else
+			{
+            Console.WriteLine("сумма элементов массива: "+myArray.Sum());
+			}
+			*/
+        }
+	}
+}
